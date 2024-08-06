7e7888cc92748637ce2754c9b5f9148750c86f9d
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-08-06 22:56:24 +0200

    add basic code to login.html

--- news/views.py
+++ news/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 from .news_api import get_news
 
 # Create your views here.
@@ -18,31 +17,3 @@ def home(request):
         'page': page,
         'has_more': len(articles) == 20 
     })
-
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'news/register.html', {'form': form})
-
-def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'news/login.html', {'form': form})
